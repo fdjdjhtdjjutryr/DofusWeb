11ec7c8f35d1d46baa3e72d3d9df2bee575057ba
Oscar_Gonzalez <ogonzalez@ing.ucsc.cl>
2026-06-29 09:03:27 -0400

    fix carpeta acciones de Misiones

--- Mantenedores/Misiones/Acciones/create.php
+++ Mantenedores/Misiones/Acciones/create.php
@@ -1,8 +1,9 @@
 <?php
     require_once dirname(__DIR__,3) . "/bootstrap.php";
+    $id_logro=$_POST["id_logro"];
     $nombre_mision=$_POST["nombre_mision"];
 
-    $consulta = "INSERT INTO misiones(nombre_mision) VALUES ('$nombre_mision')";
+    $consulta = "INSERT INTO misiones(id_logro,nombre_mision) VALUES ('$id_logro','$nombre_mision')";
     $resultado = mysqli_query($conexion , $consulta);
 
     header("Location: " . BASE_URL . "/Mantenedores/Misiones/Vistas/listar.php");
--- Mantenedores/Misiones/Acciones/update.php
+++ Mantenedores/Misiones/Acciones/update.php
@@ -1,9 +1,10 @@
 <?php
     require_once dirname(__DIR__,3) . "/bootstrap.php";
     $id_mision = $_POST["id_mision"];
+    $id_logro = $_POST["id_logro"];
     $nombre_mision=$_POST["nombre_mision"];
 
-    $consulta = "UPDATE misiones SET nombre_mision = '$nombre_mision' WHERE id_mision = '$id_mision';";
+    $consulta = "UPDATE misiones SET id_logro = '$id_logro',nombre_mision = '$nombre_mision' WHERE id_mision = '$id_mision';";
     $resultado = mysqli_query($conexion , $consulta);
 
     header("Location: " . BASE_URL . "/Mantenedores/Misiones/Vistas/listar.php");
