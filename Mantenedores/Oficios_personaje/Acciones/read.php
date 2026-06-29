a2249c8e60cf76d8158899cd4b2b97e3c8ed56cb
Oscar_Gonzalez <ogonzalez@ing.ucsc.cl>
2026-06-29 12:35:35 -0400

    add listar.php y pequeno fix de read.php

--- Mantenedores/Oficios_personaje/Acciones/read.php
+++ Mantenedores/Oficios_personaje/Acciones/read.php
@@ -2,7 +2,13 @@
 
 require_once dirname(__DIR__,3) . "/bootstrap.php";
 
-$consulta = "SELECT * FROM oficio_personaje";
+$consulta = "SELECT op.id_personaje,
+                    op.id_oficio,
+                    op.nivel_oficio,
+                    p.nombre_personaje,
+                    o.nombre_oficio
+            FROM oficio_personaje op JOIN personaje p ON op.id_personaje = p.id_personaje
+                                    JOIN oficios o ON op.id_oficio = o.id_oficio";
 $resultado = mysqli_query($conexion, $consulta);
 
 ?>
